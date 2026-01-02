8df24449fa9d699a2cbe34efe1415a5c262ea6d6
Nikita Shinov <n.shinov@a1qa.com>
2026-01-02 13:56:47 +0100

    replaced UI test with an API one

---
+++ src/test/java/tests/CreateUserTest.java
@@ -0,0 +1,39 @@
+package tests;
+
+import io.restassured.http.ContentType;
+import models.User;
+import org.apache.http.HttpStatus;
+import org.testng.annotations.Test;
+import utils.RandomUtils;
+
+import static io.restassured.RestAssured.given;
+
+public class CreateUserTest {
+    private static final String API_URL = "https://petstore.swagger.io";
+    private static final String BASE_PATH = "/v2";
+    private static final String CREATE_USER_ENDPOINT = "/user";
+
+
+    @Test(description = "Create a user - 200 Success")
+    public void createUser() {
+        User user = new User();
+        user.setUsername(RandomUtils.getRandomAlphabeticString());
+        user.setFirstName(RandomUtils.getRandomAlphabeticString());
+        user.setLastName(RandomUtils.getRandomAlphabeticString());
+        user.setEmail(RandomUtils.getRandomAlphabeticString());
+        user.setPassword(RandomUtils.getRandomAlphabeticString());
+        user.setPhone(RandomUtils.getRandomAlphabeticString());
+        user.setUserStatus(0);
+
+        given()
+                .baseUri(API_URL)
+                .basePath(BASE_PATH)
+                .contentType(ContentType.JSON)
+                .body(user)
+                .when()
+                .post(CREATE_USER_ENDPOINT)
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK);
+    }
+}
