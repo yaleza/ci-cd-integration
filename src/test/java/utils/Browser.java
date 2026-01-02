50c6f028f2883c94cac4572c4f142cf9279f182b
Nikita Shinov <n.shinov@a1qa.com>
2026-01-02 11:45:28 +0100

    changed browser options set up for jenkins ci/cd

--- src/test/java/utils/Browser.java
+++ src/test/java/utils/Browser.java
@@ -2,20 +2,30 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Browser {
     private static WebDriver driverInstance = null;
 
     public static WebDriver getInstance() {
         if (driverInstance == null) {
-            driverInstance = new ChromeDriver();
-            driverInstance.manage().window().maximize();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments(
+                    "--headless=new",
+                    "--no-sandbox",
+                    "--disable-dev-shm-usage",
+                    "--disable-gpu",
+                    "--window-size=1920,1080"
+            );
+            driverInstance = new ChromeDriver(options);
         }
         return driverInstance;
     }
 
     public static void quitDriver(){
-        driverInstance.quit();
-        driverInstance = null;
+        if (driverInstance != null) {
+            driverInstance.quit();
+            driverInstance = null;
+        }
     }
 }
