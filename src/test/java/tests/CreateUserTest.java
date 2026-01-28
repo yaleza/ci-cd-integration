50cbdbf334244ce38f1055830952d61c142e9198
Liza <jysdga>
2026-01-28 20:59:11 +0400

    Edited Name, Lastname

--- src/test/java/tests/CreateUserTest.java
+++ src/test/java/tests/CreateUserTest.java
@@ -23,8 +23,8 @@ public class CreateUserTest {
     @Test(description = "Create a user - 200 Success")
     public void createUser() {
         User user = new User();
-        user.setFirstName("Vera");
-        user.setLastName("Lyublina");
+        user.setFirstName("Liza");
+        user.setLastName("Kameneva");
         user.setLastName(RandomUtils.getRandomAlphabeticString());
         user.setEmail(RandomUtils.getRandomAlphabeticString());
         user.setPassword(RandomUtils.getRandomAlphabeticString());
